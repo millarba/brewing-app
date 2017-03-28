60380de93b020e93c6c8d575a291eea829e7151b
millarba <millarba@gmail.com>
2017-03-27 19:23:22 -0500

    bold header text and center forms

--- app/assets/javascripts/app_Controller.js
+++ app/assets/javascripts/app_Controller.js
@@ -26,14 +26,12 @@
     };
 
 //variables for the timer and temperature
-    $scope.progress_count = 0;
+    $scope.progressCount = 0;
     var tempArray = [];
     var currentTemp = "";
     $scope.completionPercentage = 0;
 
     $scope.startTimer = function() {
-      var time = document.getElementById('hidden').innerHTML;
-      console.log(time);
 //every second add time to timer, fill progress bar, and push new temp to array
       var progressInterval = $interval(function(){
           $http.get("https://api.particle.io/v1/devices/360041000a47353138383138/brew_tmp?access_token=4652099c02c9beddddfbf6ff7f74ebd88c02825b").then(function(response) {
@@ -44,12 +42,28 @@
             currentTemp = response.data.result;
             $scope.message = tempArray;
           })
-          $scope.progress_count += parseFloat(time);
-          $scope.completionPercentage += Math.round(time);
-          console.log($scope.progress_count);
-          if ($scope.progress_count >= 100) { 
+
+          $scope.progressCount += 1;
+          $scope.completionPercentage += 1;
+          console.log($scope.progressCount);
+
+          if ($scope.progressCount >= 100) { 
             $interval.cancel(progressInterval); }
         }, 1000);
+    };
+
+//changing boilProgress and boilTime to 1 for demo
+//use parseFloat(time) and Math.round(time) respectively in production          
+    $scope.boilProgress = 0;
+
+    $scope.startBoil = function() {
+      var boilTime = document.getElementById('hidden').innerHTML;
+      var boilInterval = $interval(function(){
+        $scope.boilProgress += 1;
+        if ($scope.boilProgress >= 100) {
+          $interval.cancel(boilInterval);
+        };
+      }, 1000);
     };
 
 
